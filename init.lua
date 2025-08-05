a963ccc26adeab1efa8ec295b5540e9fe7085750
thgnommy <th.brandoli@gmail.com>
2025-08-05 15:16:49 +0200

    delete comments

--- init.lua
+++ init.lua
@@ -887,29 +887,11 @@ require('lazy').setup({
     },
   },
 
-  -- { -- You can easily change to a different colorscheme.
-  --   -- Change the name of the colorscheme plugin below, and then
-  --   -- change the command in the config to whatever the name of that colorscheme is.
-  --   --
-  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --   'folke/tokyonight.nvim',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   config = function()
-  --     ---@diagnostic disable-next-line: missing-fields
-  --     require('tokyonight').setup {
-  --       styles = {
-  --         comments = { italic = true }, -- Disable italics in comments
-  --       },
-  --     }
+  -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
   --
-  --     -- Load the colorscheme here.
-  --     -- Like many other themes, this one has different styles, and you could load
-  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --     vim.cmd.colorscheme 'catppuccin-mocha'
-  --   end,
-  -- },
-
-  --  NOTE Actually override the colorscheme to use the Catppuccin theme
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   {
     'catppuccin/nvim',
     name = 'catppuccin',
@@ -918,6 +900,9 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('catppuccin').setup {
+        -- Load the colorscheme here.
+        -- Like many other themes, this one has different styles, and you could load
+        -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
         vim.cmd.colorscheme 'catppuccin-mocha',
       }
     end,
