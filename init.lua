9da09ebfdd954be1ff886c2a398202c1cf48d5e0
Thomas Brandoli <thomas.brandoli@zuru.com>
2025-11-05 09:21:29 +0100

    added diagnostic keymap

--- init.lua
+++ init.lua
@@ -455,6 +455,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+      vim.keymap.set('n', '<leader>ad', function()
+        vim.diagnostic.open_float(nil, { focus = false, border = 'rounded' })
+      end, { desc = 'Show diagnostics in floating window' })
+
       vim.keymap.set('n', '<leader>ar', builtin.lsp_references, { desc = '[S]earch References' })
       vim.keymap.set('n', '<leader>ai', builtin.lsp_implementations, { desc = '[S]earch Implementations' })
       vim.keymap.set('n', '<leader>ao', '<cmd>LspClangdSwitchSourceHeader<CR>', { desc = '[S]witch between header/cpp' })
